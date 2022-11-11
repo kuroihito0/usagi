5b2e17e76235988d8d37823959bf761acfc66603
Jarigyani <98578563+Jarigyani@users.noreply.github.com>
2022-11-11 05:42:29 +0000

    fix

--- src/page/NotFound.tsx
+++ src/page/NotFound.tsx
@@ -1,22 +1,19 @@
 import React, { useEffect, useRef } from 'react';
 const NotFound = () => {
-    const videoRef = useRef<HTMLVideoElement>(null);
-    useEffect(() => {
-        videoRef.current?.play();
-    }, []);
-    return (
-        
-        <>
-            <div> おさるさんだ！🐵🐵</div>
-            <React.StrictMode>
-                <video controls muted ref={videoRef} >
-                    <source src={video_mp4} type="video/mp4" />
-                    <p>Your browser doesn't support HTML5 video.</p>
-                </video>
-            </React.StrictMode>
-
-        </>
-    );
+  const videoRef = useRef<HTMLVideoElement>(null);
+  useEffect(() => {
+    videoRef.current?.play();
+  }, []);
+  return (
+    <>
+      <div> おさるさんだ！🐵🐵</div>
+      <React.StrictMode>
+        <video controls muted ref={videoRef}>
+          <p>Your browser doesn't support HTML5 video.</p>
+        </video>
+      </React.StrictMode>
+    </>
+  );
 };
 
 export default NotFound;
