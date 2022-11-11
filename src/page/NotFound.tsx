40547d88ac445375510115591d5bd690ad25dc61
kuroihito0 <hikaru9g@gmail.com>
2022-11-11 14:48:53 +0900

    fix

--- src/page/NotFound.tsx
+++ src/page/NotFound.tsx
@@ -1,23 +1,12 @@
 import { Link } from "react-router-dom";
-import Cockkirby from "./pics/Cockkirby.gif";
-import React, { useRef, useEffect } from 'react'
-import video_mp4 from "./pics/Cockkirby.mp4"
+
+
 const NotFound = () => {
-    const videoRef = useRef<HTMLVideoElement>(null);
-    useEffect(() => {
-        videoRef.current?.play();
-    }, []);
+
     return (
         
         <>
             <div> おさるさんだ！🐵🐵</div>
-            <React.StrictMode>
-                <video controls muted ref={videoRef} >
-                    <source src={video_mp4} type="video/mp4" />
-                    <p>Your browser doesn't support HTML5 video.</p>
-                </video>
-            </React.StrictMode>
-
         </>
     );
 };
